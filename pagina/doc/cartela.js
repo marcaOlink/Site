ae0e50939849617e73ab60ad530e3cce0d9d9cc8
Eta3 <eliasvieira080@gmail.com>
2021-08-04 20:15:12 -0300

    apache

---
+++ pagina/doc/cartela.js
@@ -0,0 +1,37 @@
+// aula 9 executamdo if e else com multimo retorno
+// sistema executa o elemento da variavel
+//comando if() ou else e else if() 
+//operação SIM OU NÃO
+
+/*var nota=19;
+var res;
+if(nota>=95){
+	res="Aprovado com louvor";
+}else if(nota >=60){
+	res="Aprovado";
+}else if(nota>=40){
+	res="Recuperação";
+}else if(nota>=20){
+	res="Reprovado";
+}else{
+	res="Reprovado com lovor";
+}
+ document.write(res);*/
+ 
+ // colhe o nome do usuario if true ou false
+ // ser nome for true pagina cliente se for false pagina plincipal
+ 
+ //var nome=prompt("seu nome é?");
+ //var cliente=confirm(nome + " " + "Você deseja continua?");
+ //var pag;
+ 
+ //if((nome+true)&&(cliente=true)||((nome=true)&&(cliente=true))
+//){
+	 
+	//  pag="pagina cliente";
+ //}else{
+	// pag="pagina plincipal";
+ //}
+  //document.write(pag);
+ var cinquentaUm=document.getElementById("nomeCinquentaUm").innerHTML;
+ 
